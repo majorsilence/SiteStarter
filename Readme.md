b9290ead56111244936e3854a3a27606dca4a17e
Peter Gill <peter.gill@townsuite.com>
2016-11-01 10:15:48 -0230

    initial commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Build
+
+```bash
+npm install
+gulp bower
+gulp copyfiles
+```
+
+
+# Add new javascript library
+
+```bash
+bower install TheBowerPackageName
+```
+
+Add as submodule and reference js libraries as needed.
---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp        = require('gulp'),
+    bower = require('gulp-bower');
+
+gulp.task('copyfiles', function() {
+   gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,woff2,eof,svg}')
+   .pipe(gulp.dest('./build/fonts'));
+
+   gulp.src('./bower_components/bootstrap/dist/css/*.min.css')
+   .pipe(gulp.dest('./build/css'));
+
+   gulp.src('./bower_components/bootstrap/dist/fonts/*.{ttf,woff,woff2,eof,svg}')
+   .pipe(gulp.dest('./build/fonts'));
+
+   gulp.src('./bower_components/bootstrap/dist/js/*.min.js')
+   .pipe(gulp.dest('./build/js'));
+
+   gulp.src('./bower_components/jquery/dist/*.min.js')
+   .pipe(gulp.dest('./build/js'));
+
+   gulp.src('./bower_components/jquery-pjax/dist/jquery.pjax.js')
+   .pipe(gulp.dest('./build/js'));
+
+   gulp.src('./bower_components/kendo-ui/js/kendo.ui.core.min.js')
+   .pipe(gulp.dest('./build/js'));
+
+   gulp.src('./bower_components/kendo-ui/styles/kendo.common.min.css')
+   .pipe(gulp.dest('./build/css'));
+
+   gulp.src('./bower_components/kendo-ui/styles/kendo.common-bootstrap.core.min.css')
+   .pipe(gulp.dest('./build/css'));
+
+   gulp.src('./bower_components/kendo-ui/styles/kendo.default.min.css')
+   .pipe(gulp.dest('./build/css'));
+
+   gulp.src('./bower_components/notifyjs/dist/notify.js')
+   .pipe(gulp.dest('./build/js'));
+
+   gulp.src('./bower_components/nprogress/nprogress.js')
+   .pipe(gulp.dest('./build/js'));
+
+   gulp.src('./bower_components/nprogress/nprogress.css')
+   .pipe(gulp.dest('./build/css'));
+
+});
+
+
+gulp.task('bower', function() {
+  return bower();
+});
+
+
+
